bc64e7f9cbe8ea5b739b5319d37bc0e33c5f1cc5
mbadar6398 <mbadar6398@gmail.com>
2019-04-07 16:20:07 +0700

    features/topic: Laying out & Styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Topic from './Topic';
 
 class App extends Component {
     render() {
         return(
-            <div><h1>PAGE</h1></div>
+            <main>
+                <section className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-md-8 col-lg-6">
+                            <Topic/>
+                        </div>
+                    </div>
+                </section>
+            </main>
         )
     }
 }
---
+++ src/components/Topic.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faThumbsDown , faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faThumbsUp, faThumbsDown)
+
+class Topic extends Component {
+    render() {
+        return(
+            <div class="topic-list">
+                <div className="card">
+                    <div className="card__header">
+                        <h4 className="card__header__title">What is Lorem Ipsum?</h4>
+                    </div>
+                    <div className="card__body">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    </div>
+                    <div className="card__footer">
+                        <ul className="card__footer__interaction">
+                            <li className="text-success"><FontAwesomeIcon icon="thumbs-up" /> 7,250</li>
+                            <li className="text-danger"><FontAwesomeIcon icon="thumbs-down" /> 11,210</li>
+                        </ul>
+                        <div className="card__footer__btn-group">
+                            <button className="btn btn--success"><FontAwesomeIcon icon="thumbs-up" /></button>
+                            <button className="btn btn--danger"><FontAwesomeIcon icon="thumbs-down" /></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__header">
+                        <h4 className="card__header__title">What is Lorem Ipsum?</h4>
+                    </div>
+                    <div className="card__body">
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    </div>
+                    <div className="card__footer">
+                        <ul className="card__footer__interaction">
+                            <li className="text-success"><FontAwesomeIcon icon="thumbs-up" /> 7,250</li>
+                            <li className="text-danger"><FontAwesomeIcon icon="thumbs-down" /> 11,210</li>
+                        </ul>
+                        <div className="card__footer__btn-group">
+                            <button className="btn btn--success"><FontAwesomeIcon icon="thumbs-up" /></button>
+                            <button className="btn btn--danger"><FontAwesomeIcon icon="thumbs-down" /></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Topic;
